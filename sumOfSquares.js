f1e0d5852ebce5574482aae00abed15964089aa5
Raj Parayane <raj@mobiuso.com>
2024-07-10 00:36:04 +0530

    Practise question's question no.8

---
+++ sumOfSquares.js
@@ -0,0 +1,27 @@
+let arr = [1,2,3,4];
+console.log(arr);
+
+let sumOfSquares = (arr, a, b) =>
+{
+    var sum = 0;
+    var a = 0;
+    var b = 0;
+    for(let i=0; i<arr.length; i++)
+    {
+
+        if(i%2==0)
+        {
+            a = arr[i];
+            b = arr[i+1];
+
+            sum += a*a + b*b;
+            // console.log(a , b, sum);
+
+            sumOfSquares(a, b);
+        }
+    }
+    console.log(sum);
+
+}
+sumOfSquares(arr);
+// console.log(sum);
