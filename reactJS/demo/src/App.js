20871bb999088e1c01f69d46d5f72041767b715b
Raj Parayane <raj@mobiuso.com>
2024-10-13 17:28:32 +0530

    Create a scrollable table with a sticky header
    - Implemented a table with scrollable rows
    - The header remains fixed while scrolling
    - Learned how to modify/override Bootstrap styles with custom CSS

--- reactJS/demo/src/App.js
+++ reactJS/demo/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Button_Event from "./EventHandlingExample/InputChangeOnButtonClick/input-change-dispay-in-console-on-buttonClick-without-hook";
+// import Button_Event from "./EventHandlingExample/InputChangeOnButtonClick/input-change-dispay-in-console-on-buttonClick-without-hook";
+
+import SheetDataTable from "./SpreadsheetToWebTable/display-spreadsheetData";
 
 function App()
 {
     return(
-        <Button_Event />
+        <SheetDataTable />
+        
     );
 }
 export default App;
